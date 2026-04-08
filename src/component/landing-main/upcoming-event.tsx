12d4c371558b1d4680b5ee8356b36857d37e657c
Lakshay Garg <lakgarg2002@gmail.com>
2026-04-08 12:54:17 -0700

    pushed latest event title

--- src/component/landing-main/upcoming-event.tsx
+++ src/component/landing-main/upcoming-event.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const UpcomingEvent = () => {
   return (
-    <div className=" h-screen md:h-[600px] flex justify-center items-center bg-[#212325]">
+    <div className=" h-screen md:h-[600px] flex flex-col justify-center gap-6 items-center bg-[#212325]">
+      <h2 className="text-5xl mb-4 transition-all duration-700 text-center text-white">
+        Upcoming Event
+      </h2>
       <iframe
         src="https://luma.com/embed/calendar/cal-MXXaoFnWHFbRyUf/events"
         width="100%"
